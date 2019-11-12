c959ddab34dd01f7765bf4029fcd10d98df82ac1
tancos02 <paulussiahaan02@gmail.com>
2019-11-12 20:54:05 +0700

    Adding comment

--- src/server.py
+++ src/server.py
@@ -16,15 +16,22 @@ def client_left(client, server):
 # Dipanggil ketika client mengirim data ke server
 def message_received(client, server, message):
 	if '!echo' in message:
+		# Mengurus kasus 1, dimana client mengirim !echo <message> 
+		# Server mengirim ulang <message>
 		server.send_message(client, message[6:], OPCODE_TEXT)
 		print("Client(%d) said: %s" % (client['id'], message))
 	elif '!submission' in message:
+		# Mengurus kasus 2, dimana client mengirim !submission 
+		# Server mengirim berkas zip berisi source code dan readme
 		data_file = b''
 		with open('Bariancrot.zip', "rb") as f:
 			data_file = f.read()
 		server.send_message(client, data_file, OPCODE_BINARY)
 		print("Client(%d) said %s" % (client['id'], message))
 	else:
+		# Mengurus kasus 3, dimana client mengirim berkas zip dari kasus 2
+		# Server mengirim 1 jika md5 checksum file yang diterima = dengan file yang dikirim
+		# mengirim 0 jika berbeda
 		message_hash = hashlib.md5(message).hexdigest()
 		
 		with open('Bariancrot.zip', "rb") as f:
